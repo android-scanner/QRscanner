df132c1f24d146cd086d16bbdf45c65502e8a0a1
wiam47 <wiam67bissi@gmail.com>
2021-09-09 18:51:30 +0100

    databinding

--- app/src/main/java/com/example/qrcodescanner/ui/CinActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/ui/CinActivity.kt
@@ -7,19 +7,24 @@ import android.text.TextWatcher
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
-import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.qrcodescanner.R
+import com.example.qrcodescanner.databinding.ActivityCinBinding
 
 const val intentCin = "cin"
 
 class CinActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCinBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cin)
+        binding = ActivityCinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val cinTxt = findViewById<EditText>(R.id.txtBoxCIN)
-        val submitBtn = findViewById<Button>(R.id.btnSubmitCIN)
+        val cinTxt = binding.txtBoxCIN
+        val submitBtn = binding.btnSubmitCIN
         cinTxt.addTextChangedListener(object : TextWatcher {
             override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
                 submitBtn.isEnabled = s.toString().trim { it <= ' ' }.isNotEmpty()
